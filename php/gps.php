6862e712b69ac6ed87529ed05ec03a9b246c9a12
charlesvg <charles.vangoidsenhoven@gmail.com>
2016-04-17 20:39:28 +0200

    using mysql

--- php/gps.php
+++ php/gps.php
@@ -25,12 +25,12 @@ if( true )
   $date = DateTime::createFromFormat('YmdHis.u', "20160417175759.000",new DateTimeZone('UTC'));
   echo $date->format("Y-m-d H:i:s");
   
-  $utc = mysql_real_escape_string($date->format("Y-m-d H:i:s"));
-  $lat = mysql_real_escape_string('1.1');
-  $lon = mysql_real_escape_string('1.2');
-  $alt = mysql_real_escape_string('1.3');
-  $spd = mysql_real_escape_string('1.4');
-  $crs = mysql_real_escape_string('1.5');
+  $utc = $link->real_escape_string($date->format("Y-m-d H:i:s"));
+  $lat = $link->real_escape_string('1.1');
+  $lon = $link->real_escape_string('1.2');
+  $alt = $link->real_escape_string('1.3');
+  $spd = $link->real_escape_string('1.4');
+  $crs = $link->real_escape_string('1.5');
   
   
 
