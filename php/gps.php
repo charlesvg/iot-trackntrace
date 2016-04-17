7b6ae680a3f7c8ae0cea53b5966e19db91de4577
charlesvg <charles.vangoidsenhoven@gmail.com>
2016-04-17 17:05:43 +0200

    testing other dump method

--- php/gps.php
+++ php/gps.php
@@ -1,7 +1,11 @@
 <?php
+/*
 $req_dump = print_r($_POST, TRUE);
 $fp = fopen('request.log', 'a');
 fwrite($fp, $req_dump);
 fclose($fp);
+*/
+<?php
+file_put_contents("post.log",print_r($_POST,true));
 ?>
 OK
